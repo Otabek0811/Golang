1fee1cabbb961d8c6686f2090d47044c72a1a6f5
Otabek0811 <karimjonov6006@gmail.com>
2022-04-25 18:15:48 +0500

    Added new function for calculating

---
+++ 1.go
@@ -0,0 +1,22 @@
+package main
+import "fmt"
+func main(){
+    var a int
+    fmt.Scan(&a)
+    if a<10{
+        fmt.Println(a)
+    }else if a>9{
+        a=a/10
+    }else if a<10{
+        fmt.Println(a)
+    }else if a>9{
+        a=a/10
+    }else if a<10{
+        fmt.Println(a)
+    }else if a>9{
+        a=a/10
+    }else if a<10{
+        fmt.Println(a)
+    }
+    
+}
