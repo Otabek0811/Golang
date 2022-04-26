17fdc0b7705ba536759136c2d6a346cf3be908a5
Otabek0811 <karimjonov6006@gmail.com>
2022-04-26 17:54:09 +0500

    one

---
+++ first/1.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func massiveFunc(s string) string{
+    newString:=s+" "+"World"
+    return newString
+}
+
+func main(){
+    newString := massiveFunc("Hello")
+    fmt.Println(newString)
+    fmt.Println("helmiojknml")
+  
+    
+}
